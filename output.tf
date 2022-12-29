b249ccfc70fa8cc137c78054f6349ea14b517629
stomariv <107636072+stomariv@users.noreply.github.com>
2022-12-29 16:40:52 -0500

    added output

--- output.tf
+++ output.tf
@@ -1,3 +1,9 @@
-output "ingress_endpoint" {
-    value = module.wordpress-helm-chart-deploy.ingress.rules.host
+data "kubernetes_service" "ingress_svc" {
+  metadata {
+    name = "wordpress"
+  }
+}
+
+output "instance_ip_addr" {
+  value = data.kubernetes_service.ingress_svc.status.0.load_balancer.0.ingress[0].ip
 }
