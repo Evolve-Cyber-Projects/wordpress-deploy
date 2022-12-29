5cfa7eecdd2b1f2b068ec5fc9ae6b51c7f4b71fb
stomariv <107636072+stomariv@users.noreply.github.com>
2022-12-29 18:01:55 -0500

    removed output

--- output.tf
+++
@@ -1,9 +0,0 @@
-data "kubernetes_ingress" "example" {
-  metadata {
-    name = "wordpress"
-  }
-}
-output "instance_ip_addr" {
-     value = data.kubernetes_ingress.example.ingress.0.hostname
-   #value = data.kubernetes_service.ingress_svc.status.0.load_balancer.0.ingress[0].ip
-}
