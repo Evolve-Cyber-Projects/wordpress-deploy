1169aca64349732f007307d60e218903b8f7bd9f
stomariv <107636072+stomariv@users.noreply.github.com>
2022-12-30 17:13:23 -0500

    edit output

--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
 output "ingress_endpoint" {
-    value = module.wordpress-helm-chart-deploy.ingress_endpoint
-}
+    value = module.wordpress-helm-chart-deploy.status.0.load_balancer.0.ingress.0.hostname #ingress_endpoint
+} 
+# value = data.kubernetes_ingress_v1.wordpress.status.0.load_balancer.0.ingress.0.hostname
