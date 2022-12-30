924c7626fd1b3609fd98bcaf11003f0237ab3901
stomariv <107636072+stomariv@users.noreply.github.com>
2022-12-30 17:28:11 -0500

    added output

--- modules/app-helm/output.tf
+++ modules/app-helm/output.tf
@@ -1,10 +1,10 @@
-data "kubernetes_ingress_v1" "wordpress" {
-  metadata {
-    name      = helm_release.helm_deployment.namespace
-    namespace = helm_release.helm_deployment.namespace
-  }
-}
+# data "kubernetes_ingress_v1" "wordpress" {
+#   metadata {
+#     name      = helm_release.helm_deployment.namespace
+#     namespace = helm_release.helm_deployment.namespace
+#   }
+# }
 
-output "ingress_endpoint" {
-    value = data.kubernetes_ingress_v1.wordpress.status.0.load_balancer.0.ingress.0.hostname
-}
+# output "ingress_endpoint" {
+#     value = data.kubernetes_ingress_v1.wordpress.status.0.load_balancer.0.ingress.0.hostname
+# }
--- output.tf
+++ output.tf
@@ -1,4 +1,18 @@
+# output "ingress_endpoint" {
+#     value = module.wordpress-helm-chart-deploy.status.0.load_balancer.0.ingress.0.hostname #ingress_endpoint
+# } 
+# # value = data.kubernetes_ingress_v1.wordpress.status.0.load_balancer.0.ingress.0.hostname
+
+data "kubernetes_resource" "example" {
+  api_version = "v1"
+  kind        = "Ingress"
+
+  metadata {
+    name      = "wordpress"
+    namespace = "wordpress"
+  }
+}
+
 output "ingress_endpoint" {
-    value = module.wordpress-helm-chart-deploy.status.0.load_balancer.0.ingress.0.hostname #ingress_endpoint
-} 
-# value = data.kubernetes_ingress_v1.wordpress.status.0.load_balancer.0.ingress.0.hostname
+    value = data.kubernetes_resource.example.spec.0.hostname
+}
