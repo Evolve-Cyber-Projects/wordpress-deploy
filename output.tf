1b83478e070abc6a6d1f5939008661021cb89446
stomariv <107636072+stomariv@users.noreply.github.com>
2022-12-30 12:12:59 -0500

    changed output

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 data "kubernetes_ingress_v1" "argocd" {
   metadata {
     name      = "argocd-server"
-    namespace = module.wordpress-helm-chart-deploy.namespace
+    namespace = module.wordpress-helm-chart-deploy.deployment_namespace
   }
 }
