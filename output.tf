64ca82302b55436147d152608442cea61fbf80ea
stomariv <107636072+stomariv@users.noreply.github.com>
2023-01-02 12:26:36 -0500

    output

--- output.tf
+++ output.tf
@@ -10,5 +10,5 @@ data "kubernetes_ingress_v1" "ingress" {
 
 output "url" {
   value = data.kubernetes_ingress_v1.ingress.spec[0].rule[0].host
-  
+}  
   #  data.kubernetes_ingress.ingress[0].spec[0].tls[0].hosts
